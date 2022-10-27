1e358ffddefbd22cb9dab5fc45e366dd19ab2d09
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-10-27 13:55:38 -0300

    criado storeitem, data e util

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,30 @@
+import { Card } from "react-bootstrap";
+import { formatCurrency } from "../util/formatCurrency";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  return (
+    <Card>
+      <Card.Img 
+        variant="top" 
+        src={imgUrl} 
+        height="200px" 
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+
+  )
+
+}
