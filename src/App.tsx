af2a63b3277b9c64cc2e442bde5c10cada865fd0
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-10-27 15:17:23 -0300

    criado cartItem e shoppingCart

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
   );
 }
 
